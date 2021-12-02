0dea0352cd29028c78d1666baed7b6c8f1a45505
Aminebneji <aminebneji@gmail.com>
2021-12-02 22:34:14 +0100

    les cours du soirs
    02/12/2021

---
+++ js/index.js
@@ -0,0 +1,121 @@
+// let scene;
+// let sceneEnCours = 1; 
+// let PdV = 20;  
+// let textLiaison = "";
+
+// let urlJSON = "./data/test.json";
+
+// async function fetchInfo() {
+//     fetch(urlJSON)
+//         .then(response => response.json())
+//         .then(data => scene = data.Scene)
+//         .catch(error => console.log(error));
+//     setTimeout(() => { main() }, 150);
+// }
+
+// fetchInfo();
+
+// function main (){
+//     console.log(scene[1].Choix);
+//     majScene() ; 
+// }
+
+// function majFullChoix(){
+
+// }
+
+// function majUnChoix(num){
+
+//         const choix = document.getElementById('choix'+(num+1));
+//         choix.textContent= scene[sceneEnCours].Choix[num].Texte;
+//         choix.hidden = false;
+
+// } 
+
+// function majScene(){
+//     let nbChoix = scene[sceneEnCours].Choix.length; 
+//     console.log(nbChoix) ; 
+//     console.log(scene[sceneEnCours].Choix[0].Texte);
+//     for(i=0 ; i < nbChoix ; i++){
+//         majUnChoix(i);
+//     } 
+//     // for(i= nbChoix; i<4 ; i++){ 
+//     //     const choix = document.getElementById('choix'+(i));
+//     //     choix.textContent= scene[sceneEnCours].Choix[i].Texte;
+//     // }
+//    const histoire = document.getElementById("content");
+//    histoire.textContent = scene[sceneEnCours].Description; 
+
+// }  
+
+// // ------------------------------------------------------------------------------------------------------
+
+//  // Boucler sur les choix
+//     // scene[1].Choix.forEach(choix => {
+//     //     console.log(choix.Texte)
+//     // });
+
+//     // Selctionner une balise et y mettre la Decription de la scene
+//     // let p = document.querySelector("p");
+//     // p.textContent = scene[1].Description
+
+
+
+let scene;
+let sceneEnCours = 1;
+let PdV = 20;
+let textLiaison = "";
+
+let urlJSON = "./data/data.json";
+
+async function fetchInfo() {
+    fetch(urlJSON)
+        .then(response => response.json())
+        .then(data => scene = data.Scene)
+        .catch(error => console.log(error));
+    setTimeout(() => { main() }, 150);
+}
+
+fetchInfo();
+
+function main() {
+    console.log(scene[1].Choix);
+    majScene();
+}
+
+
+
+function majUnChoix(num) {
+
+    const choix = document.getElementById('choix' + (num + 1));
+    choix.textContent = scene[sceneEnCours].Choix[num].Texte;
+    choix.addEventListener("click", function(){
+        sceneEnCours = scene[sceneEnCours].Choix[num].Vers;
+        majScene();
+    } );
+    
+    choix.hidden = false; 
+}
+function majFullChoix() {
+    let nbChoix = scene[sceneEnCours].Choix.length;
+    for (i = 0; i < nbChoix; i++) {
+        majUnChoix(i);
+    }
+    for (i = nbChoix + 1; i < 4; i++) {
+        const choix = document.getElementById('choix' + (i));
+        choix.hidden = true;
+    }
+
+}
+
+function majScene() {
+    // // console.log(nbChoix);
+    // console.log(scene[sceneEnCours].Choix[0].Texte);
+    majFullChoix();
+    const histoire = document.getElementById("content");
+    histoire.textContent = scene[sceneEnCours].Description;
+} 
+
+
+
+
