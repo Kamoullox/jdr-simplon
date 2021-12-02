ca13ecf2520fc3a06e46f15edba26c703cc68f5f
Kamoullox <boucetta.kamel62@gmail.com>
2021-12-02 15:11:40 +0100

    add json file structure

--- js/index.js
+++ js/index.js
@@ -1,101 +1,26 @@
-let allScenes = [];
+let scene;
 
 let urlJSON = "./data/test.json";
-let urlXML = "./data/test.xml";
 
-// --------------------structure attendu--------------------------
-// let history = [
-//     ["sceneDescription", [UserAnswer, UserAnswer, UserAnswer]],
-//     ["sceneDescription", [UserAnswer, UserAnswer]],
-//     ["sceneDescription", [UserAnswer, UserAnswer, UserAnswer]],
-//     ["sceneDescription", [UserAnswer, UserAnswer, UserAnswer]],
-//     ["sceneDescription", [UserAnswer, UserAnswer]],
-//     ["sceneDescription", [UserAnswer, UserAnswer, UserAnswer]]
-// ]
+async function fetchInfo() {
+    fetch(urlJSON)
+        .then(response => response.json())
+        .then(data => scene = data.Scene)
+        .catch(error => console.log(error));
+    setTimeout(() => { main() }, 150);
+}
 
-class UserAnswer {
-    constructor(choiceText, linkText, indexOfNextScene, lifeModification){
-        this.choiceText = choiceText;
-        this.linkText = linkText;
-        this.indexOfNextScene = indexOfNextScene;
-        this.lifeModification = lifeModification;
-    }
-};
-// -----------------------------------------------------
+fetchInfo();
 
-fetch(urlJSON)
-.then(response => response.json())
-.then(json => {
-    let allScenesJSON = json.Histoire.Scene;
+function main (){
+    console.log(scene[1].Choix);
 
-    allScenesJSON.forEach(sceneJSON => {
-        let sceneDescription = sceneJSON.Description;
-        let newScene = [sceneDescription];
+    // Boucler sur les choix
+    // scene[1].Choix.forEach(choix => {
+    //     console.log(choix.Texte)
+    // });
 
-        // console.log("Push de -> " + sceneDescription );
-
-        (sceneJSON.Choix).forEach(choice => {
-            let choiceText = choice.Texte;
-            let linkText = choice.Liaison;
-            let indexOfNextScene = choice.Vers;
-            let lifeModification = choice.PdV;
-            newScene.push(new UserAnswer(choiceText, linkText, indexOfNextScene, lifeModification))
-        });
-
-        // console.log(JSON.stringify(sceneRow))
-
-        allScenes.push(newScene);
-            
-    });
-});
-
-console.log(allScenes)
-
-// --------------------structure attendu--------------------------
-// let history = [
-//     ["scene", [UserAnswer, UserAnswer, UserAnswer]],
-//     ["scene", [UserAnswer, UserAnswer]],
-//     ["scene", [UserAnswer, UserAnswer, UserAnswer]],
-//     ["scene", [UserAnswer, UserAnswer, UserAnswer]],
-//     ["scene", [UserAnswer, UserAnswer]],
-//     ["scene", [UserAnswer, UserAnswer, UserAnswer]]
-// ]
-
-// -------------Test avec des valeurs rentré à la mains -----------
-let answers1 = new UserAnswer(
-    "Prendre le coffre de droite",
-    "Vous avez séléctionné le coffre de droite, il explose. -10 PV",
-    2,
-    -10
-);
-let answers2 = new UserAnswer(
-    "Prendre le coffre de gauche",
-    "Vous avez séléctionné le coffre de gauche, il contient une potion . +10 PV",
-    2,
-    10
-);
-
-let answerLine = ["Vous avez séléctionné le coffre de gauche, il contient une potion . +10 PV", answers1, answers2];
-let allScene2 = [];
-allScene2.push(answerLine);
-allScene2.push(answerLine);
-console.log(allScene2);
-
-// ----------------Test pour extraire les données du XML--------------------
-// let story = [];
-
-// fetch(urlXML)
-// .then(response=>response.text())
-// .then(data => {
-//     let parser = new DOMParser();
-//     let xml = parser.parseFromString(data, "application/xml");
-//     getDescription(xml);
-// })
-
-// function getDescription(x){
-//     let descriptions = x.getElementsByTagName('Description');
-//     for (let i=0; i<descriptions.length; i++){
-//         let description = descriptions[i].firstChild.nodeValue;
-//         story.push([description])
-//     }
-// }
+    // Selctionner une balise et y mettre la Decription de la scene
+    // let p = document.querySelector("p");
+    // p.textContent = scene[1].Description
+}
