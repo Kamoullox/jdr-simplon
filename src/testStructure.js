215638ba793dabaad70609ed67ddae094f218340
Hervé Go <gomanne.student@gmail.com>
2021-12-03 13:47:57 +0100

    maj

--- src/testStructure.js
+++ src/testStructure.js
@@ -1,23 +1,19 @@
-let history = [];
 //     ["sceneDescription", [UserAnswer, UserAnswer, UserAnswer]],
 
 
-class UserAnswer {
-    constructor(choiceText, linkText, indexOfNextScene, lifeModification){
-        this.choiceText = choiceText;
-        this.linkText = linkText;
-        this.indexOfNextScene = indexOfNextScene;
-        this.lifeModification = lifeModification;
-    }
-};
+
 
 //Init pour test
 function initHistory() {
-    let ua1 = new UserAnswer("Recommencer", "", 2, 100);
-    history[1] = ["Vous êtes mort !", [ua1] ];
-    let ua1 = new UserAnswer("Fuir", "Vous fuyez", 2, 0);
-    let ua2 = new UserAnswer("Combattre", "Vous perdez 7 pdv", 2, -7);
-    history[1] = ["Un ours !", [ua1, ua2] ];
-    let ua1 = new UserAnswer("Aller dans la caverne.", "", 1, 0);
-    history[2] = ["Vous sortez de la caverne.", [ua1] ];
+    let ua0 = new UserAnswer("Recommencer", "", 2, 100);
+    history[0] = "Vous êtes mort !";
+    choices[0] = ua0;
+    ua0 = new UserAnswer("Fuir", "Vous fuyez", 2, 0);
+    let ua1 = new UserAnswer("Combattre", "Vous perdez 7 pdv", 2, -7);
+    history[1] = "Un ours !"
+    choices[0] = ua0;
+    choices[1] = ua1;
+    ua0 = new UserAnswer("Aller dans la caverne.", "", 1, 0);
+    history[2] = "Vous sortez de la caverne.";
+    choices[0] = ua0;
 }
