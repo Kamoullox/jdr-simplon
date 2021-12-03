b94db4ea3c599d8636754465b9de70fba44c428d
Kamoullox <boucetta.kamel62@gmail.com>
2021-12-03 13:53:54 +0100

    Add life point mecanic

--- js/index.js
+++ js/index.js
@@ -1,69 +1,6 @@
-// let scene;
-// let sceneEnCours = 1; 
-// let PdV = 20;  
-// let textLiaison = "";
-
-// let urlJSON = "./data/test.json";
-
-// async function fetchInfo() {
-//     fetch(urlJSON)
-//         .then(response => response.json())
-//         .then(data => scene = data.Scene)
-//         .catch(error => console.log(error));
-//     setTimeout(() => { main() }, 150);
-// }
-
-// fetchInfo();
-
-// function main (){
-//     console.log(scene[1].Choix);
-//     majScene() ; 
-// }
-
-// function majFullChoix(){
-
-// }
-
-// function majUnChoix(num){
-
-//         const choix = document.getElementById('choix'+(num+1));
-//         choix.textContent= scene[sceneEnCours].Choix[num].Texte;
-//         choix.hidden = false;
-
-// } 
-
-// function majScene(){
-//     let nbChoix = scene[sceneEnCours].Choix.length; 
-//     console.log(nbChoix) ; 
-//     console.log(scene[sceneEnCours].Choix[0].Texte);
-//     for(i=0 ; i < nbChoix ; i++){
-//         majUnChoix(i);
-//     } 
-//     // for(i= nbChoix; i<4 ; i++){ 
-//     //     const choix = document.getElementById('choix'+(i));
-//     //     choix.textContent= scene[sceneEnCours].Choix[i].Texte;
-//     // }
-//    const histoire = document.getElementById("content");
-//    histoire.textContent = scene[sceneEnCours].Description; 
-
-// }  
-
-// // ------------------------------------------------------------------------------------------------------
-
-//  // Boucler sur les choix
-//     // scene[1].Choix.forEach(choix => {
-//     //     console.log(choix.Texte)
-//     // });
-
-//     // Selctionner une balise et y mettre la Decription de la scene
-//     // let p = document.querySelector("p");
-//     // p.textContent = scene[1].Description
-
-
-
 let scene;
 let sceneEnCours = 1;
-let PdV = 20;
+let life = 20;
 let textLiaison = "";
 
 let urlJSON = "./data/data.json";
@@ -78,30 +15,66 @@ async function fetchInfo() {
 
 fetchInfo();
 
+
 function main() {
-    console.log(scene[1].Choix);
+    displayLife();
     majScene();
 }
 
-function addLinkText(){
-    scene[sceneEnCours].Choix.textLiaison
+function displayLife(){
+    lifeDisplay = document.querySelector("#life");
+    lifeDisplay.textContent = life;
 }
 
+function changeLifePoint(changeLife){
+    console.log("Mise a jour des point de vie -> " + changeLife);
+    changeLife =parseInt(changeLife);
 
+    life += changeLife;
+
+    life = life > 20 ? 20 : life;
+
+   
+
+    displayLife();
+}
+
+// -----------------------------------------------------------------------------------------------------------
 function majUnChoix(num) {
+    console.log("Mise à jour du choix " + (num+1));
 
     const choix = document.getElementById('choix' + (num + 1));
     choix.textContent = scene[sceneEnCours].Choix[num].Texte;
     
-    choix.addEventListener("click", function(){
-        textLiaison = scene[sceneEnCours].Choix[num].Liaison;
-        sceneEnCours = scene[sceneEnCours].Choix[num].Vers;
-        majScene();
-    } );
-    
     choix.hidden = false; 
+    console.log("Choix " + (num+1) + " mis à jour!");
+    console.log("");
 }
+
+function clickOption(i){
+    console.log("Vous avez cliqué sur un choix qui envoie vers la scène " + scene[sceneEnCours].Choix[i].Vers)
+    textLiaison = scene[sceneEnCours].Choix[i].Liaison;
+
+    changeLifePoint(scene[sceneEnCours].Choix[i].PdV);
+
+    if (life <= 0){
+        life = 0;
+        sceneEnCours = 0;
+    }
+    else{
+        sceneEnCours = scene[sceneEnCours].Choix[i].Vers;
+    }
+
+
+
+    majScene();
+}
+
+
 function majFullChoix() {
+    console.log("Mise à jour de tous les choix en cours ...");
+    console.log("");
+
     let nbChoix = scene[sceneEnCours].Choix.length;
 
     // Change le texte de tous les choix
@@ -114,15 +87,18 @@ function majFullChoix() {
         const choix = document.getElementById('choix' + (i));
         choix.hidden = true;
     }
+    console.log("Tous les Choix sont à jour !");
+    console.log("---------------------------------------------------");
 }
+// -----------------------------------------------------------------------------------------------------------
 
 function majScene() {
-    // // console.log(nbChoix);
-    // console.log(scene[sceneEnCours].Choix[0].Texte);
-    majFullChoix();
+    console.log("Mise en place de la scène -> " + sceneEnCours);
+
     const histoire = document.getElementById("content");
-    console.log(textLiaison)
     histoire.innerHTML  = textLiaison + (textLiaison != "" ? "<br /><br />" : "")  + scene[sceneEnCours].Description;
+
+    majFullChoix();
 }
 
 
